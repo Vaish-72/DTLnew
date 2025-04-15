380df75870fc758a7a8c055cdc2ef96d9519af34
YashMarke130105 <yashmarke2005@gmail.com>
2025-04-15 15:31:00 +0530

    new line added

--- test.py
+++ test.py
@@ -9,3 +9,4 @@ if __name__ == "__main__":
     print("Hex of 255:", to_hex(255))       # Prints 0xff
     print("Decimal of 0xff:", from_hex("0xff"))  # Prints 255
     print("Hex of 16:", to_hex(16))         # Prints 0x10
+    print("Decimal of 0x10:", from_hex("0x10"))  # Prints 16
