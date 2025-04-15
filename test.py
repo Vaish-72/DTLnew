f0e3c5da0256c9ce00507def8b0214c6ff892bdf
daryappa8018 <daryappamane098@gmail.com>
2025-04-15 16:04:49 +0530

    hello added

--- test.py
+++ test.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     print("Decimal of 0xff:", from_hex("0xff"))  # Prints 255
     print("Hex of 16:", to_hex(16))         # Prints 0x10
     print("Decimal of 0x10:", from_hex("0x10"))  # Prints 16
-    //hello world   hello
+    //hello world   hello hii
